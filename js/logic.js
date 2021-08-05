c8a17468c0d6fb0d1829cccfa31b25d1391a5c00
LLYS-Phys <lachyordanov@gmail.com>
2021-08-06 00:05:21 +0300

    Add files via upload

--- js/logic.js
+++ js/logic.js
@@ -8,3 +8,22 @@ function closeMenu() {
   document.getElementById('ham').style.color="white";
 }
 
+function smart(){
+  if(document.getElementById("menu").style.width < "25vw"){
+    openMenu();
+  }
+  else if(document.getElementById("menu").style.width >= "25vw"){
+    closeMenu();
+  }
+}
+
+var opt = document.getElementById('menu').children;
+if(opt[1].selected = true){
+  document.getElementById('shows').style.display = 'block';
+}
+else{document.getElementById('shows').style.display = 'none'}
+
+//if(document.getElementById('one').clicked = true){
+//  document.getElementById('shows').style.display = 'block';
+//}
+//else{  document.getElementById('shows').style.display = 'none';}
