b7d2edcd522109d6e633481d2557449248108c9c
LLYS-Phys <lachyordanov@gmail.com>
2021-08-06 01:51:25 +0300

    Add files via upload

--- js/logic.js
+++ js/logic.js
@@ -7,23 +7,113 @@ function closeMenu() {
   document.getElementById("menu").style.width = "0";
   document.getElementById('ham').style.color="white";
 }
+var showsinfo = document.getElementById('shows');
+var newsinfo = document.getElementById('news');
+var videosinfo = document.getElementById('videos');
+var musicinfo = document.getElementById('music');
+var photosinfo = document.getElementById('photos');
+var subscribeinfo = document.getElementById('subscribe');
+var membersinfo = document.getElementById('members');
+var connectinfo = document.getElementById('connect');
 
-//function smart(){
-//  if(document.getElementById("menu").style.width < "25vw"){
-//    openMenu();
-//  }
-//  else if(document.getElementById("menu").style.width >= "25vw"){
-//    closeMenu();
-//  }
-//}
+var shows = document.getElementById('one');
+shows.addEventListener('click', function(event){
+  showsinfo.style.display = 'block';
+  newsinfo.style.display = 'none';
+  videosinfo.style.display = 'none';
+  musicinfo.style.display = 'none';
+  photosinfo.style.display = 'none';
+  subscribeinfo.style.display = 'none';
+  membersinfo.style.display = 'none';
+  connectinfo.style.display = 'none';
+})
 
-//var opt = document.getElementById('menu').children;
-//if(opt[1].selected = true){
-//  document.getElementById('shows').style.display = 'block';
-//}
-//else{document.getElementById('shows').style.display = 'none'}
+var news = document.getElementById('two');
+news.addEventListener('click', function(event){
+  showsinfo.style.display = 'none';
+  newsinfo.style.display = 'block';
+  videosinfo.style.display = 'none';
+  musicinfo.style.display = 'none';
+  photosinfo.style.display = 'none';
+  subscribeinfo.style.display = 'none';
+  membersinfo.style.display = 'none';
+  connectinfo.style.display = 'none';
+})
 
-//if(document.getElementById('one').clicked = true){
-//  document.getElementById('shows').style.display = 'block';
-//}
-//else{  document.getElementById('shows').style.display = 'none';}
+var videos = document.getElementById('three');
+videos.addEventListener('click', function(event){
+  showsinfo.style.display = 'none';
+  newsinfo.style.display = 'none';
+  videosinfo.style.display = 'block';
+  musicinfo.style.display = 'none';
+  photosinfo.style.display = 'none';
+  subscribeinfo.style.display = 'none';
+  membersinfo.style.display = 'none';
+  connectinfo.style.display = 'none';
+})
+
+var music = document.getElementById('four');
+music.addEventListener('click', function(event){
+  showsinfo.style.display = 'none';
+  newsinfo.style.display = 'none';
+  videosinfo.style.display = 'none';
+  musicinfo.style.display = 'block';
+  photosinfo.style.display = 'none';
+  subscribeinfo.style.display = 'none';
+  membersinfo.style.display = 'none';
+  connectinfo.style.display = 'none';
+})
+
+var photos = document.getElementById('five');
+photos.addEventListener('click', function(event){
+  showsinfo.style.display = 'none';
+  newsinfo.style.display = 'none';
+  videosinfo.style.display = 'none';
+  musicinfo.style.display = 'none';
+  photosinfo.style.display = 'block';
+  subscribeinfo.style.display = 'none';
+  membersinfo.style.display = 'none';
+  connectinfo.style.display = 'none';
+})
+
+var subscribe = document.getElementById('six');
+subscribe.addEventListener('click', function(event){
+  showsinfo.style.display = 'none';
+  newsinfo.style.display = 'none';
+  videosinfo.style.display = 'none';
+  musicinfo.style.display = 'none';
+  photosinfo.style.display = 'none';
+  subscribeinfo.style.display = 'block';
+  membersinfo.style.display = 'none';
+  connectinfo.style.display = 'none';
+})
+
+var members = document.getElementById('seven');
+members.addEventListener('click', function(event){
+  showsinfo.style.display = 'none';
+  newsinfo.style.display = 'none';
+  videosinfo.style.display = 'none';
+  musicinfo.style.display = 'none';
+  photosinfo.style.display = 'none';
+  subscribeinfo.style.display = 'none';
+  membersinfo.style.display = 'block';
+  connectinfo.style.display = 'none';
+})
+
+var connect = document.getElementById('eight');
+connect.addEventListener('click', function(event){
+  showsinfo.style.display = 'none';
+  newsinfo.style.display = 'none';
+  videosinfo.style.display = 'none';
+  musicinfo.style.display = 'none';
+  photosinfo.style.display = 'none';
+  subscribeinfo.style.display = 'none';
+  membersinfo.style.display = 'none';
+  connectinfo.style.display = 'block';
+})
+
+function closeTab(){
+  for(i=0; i<8; i++){
+    document.getElementsByClassName('tab')[i].style.display = 'none';
+  }
+}
