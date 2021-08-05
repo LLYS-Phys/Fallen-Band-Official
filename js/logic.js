934b9174c36ed0d3f8d4dbe7095561063bbb2250
LLYS-Phys <lachyordanov@gmail.com>
2021-08-05 16:13:52 +0300

    Add files via upload

---
+++ js/logic.js
@@ -0,0 +1,10 @@
+function openMenu() {
+  document.getElementById("menu").style.width = "15vw";
+  document.getElementById('ham').style.color='transparent';
+}
+
+function closeMenu() {
+  document.getElementById("menu").style.width = "0";
+  document.getElementById('ham').style.color="white";
+}
+
